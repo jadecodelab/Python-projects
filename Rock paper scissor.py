fa130bdeb75b70fcf0e0d7464524dee25bde0f58
jadecodelab <139015219+jadecodelab@users.noreply.github.com>
2025-11-12 20:30:11 -0800

    Modularizing code

--- Rock paper scissor.py
+++ Rock paper scissor.py
@@ -5,22 +5,22 @@
 
 import random
 
-print('Welcome to Rock Paper Scissors Game!\n')
+emojis = {'r':'✊', 's':'✌', 'p':'✋'}
+choices = ('r','p','s')
 
-while True:
-    emojis = {'r':'✊', 's':'✌', 'p':'✋'}
-    choices = ('r','p','s')
-    
-    user_choice = input('Choose rock, paper, or scissors (r/p/s): ').lower()
-    if user_choice not in choices:
-       print('Invalid choice!')
-       continue
-       
-    computer_choice = random.choice(choices)
-    
+def get_user_choice():
+    while True:
+        user_choice = input('Choose rock, paper, or scissors (r/p/s): ').lower()
+        if user_choice in choices:
+            return user_choice
+        else:
+            print('Invalid choice!')
+      
+def display_choices(user_choice, computer_choice):
     print(f'You chose {emojis[user_choice]}')
     print(f'Computer chose {emojis[computer_choice]}')
 
+def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print('Tie!')
     elif((user_choice == 'r' and computer_choice == 's') or
@@ -30,8 +30,22 @@ while True:
     else:
         print('You lose')
 
-    should_continue = input('Continue?(y/n): ').lower()
-    if should_continue == 'n':
-        print('Goodbye!')
-        break
+def play_game():
+    print('Welcome to Rock Paper Scissors Game!\n')
+
+    while True:
+        user_choice = get_user_choice()     
+        computer_choice = random.choice(choices)
+        
+        display_choices(user_choice, computer_choice)
+        determine_winner(user_choice, computer_choice)
+
+        should_continue = input('Continue?(y/n): ').lower()
+        if should_continue == 'n':
+            print('Goodbye!')
+            break
+        
+play_game()
     
+
+
